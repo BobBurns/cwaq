0d562f60aa2df609345f9353ee48aa3c5b045ff1
Bob Burns <reburns@protonmail.com>
2018-09-08 12:41:05 -0700

    first commit

---
+++ README.txt
@@ -0,0 +1,37 @@
+CLOUDWATCH ALARM QUERY - cwaq
+====
+Nagios plugin to check AWS Cloudwatch Alarm and return status. Useful for when
+ you just want to check if a predefined alarm is present on a AWS instance and
+ utilize nagios alerting capability.
+
+USAGE
+=====
+$ ./cwaq -i <instance-id> -m <metric> [-n <namespace]
+
+
+INSTALL
+=======
+Go get the latest version of GO here: https://golang.org/doc/install
+Then in cwaq directory,
+$ go get
+$ go build
+
+Make sure the server you are running this program on has the keys for the hosts
+it is checking, or for an added security benefit, run on an AWS instance and
+give the hosts a policy to allow AWS Cloudwatch access.
+
+NOTES
+=====
+You might want to play with the time period parameter to get better granularity.
+
+EXAMPLE OUTPUT
+==============
+bob@b79ce:~/godev/src/github.com/BobBurns/cwaq$ ./cwaq -i i-009614dxxxxxxx -m CPUUtilization
+OK | state=0
+bob@b79ce:~/godev/src/github.com/BobBurns/cwaq$ echo $?
+0
+bob@b79ce:~/godev/src/github.com/BobBurns/cwaq$ ./cwaq -i i-009614dxxxxxxx -m CPUUtilization
+ALARM - Threshold Crossed: 1 out of the last 1 datapoints [99.636612021858 (08/09/18 19:10:00)] was greater than or equal to the threshold (10.0) (minimum 1 datapoint for OK -> ALARM transition). | state=1
+bob@b79ce:~/godev/src/github.com/BobBurns/cwaq$ echo $?
+2
+bob@b79ce:~/godev/src/github.com/BobBurns/cwaq$
---
+++ main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/cloudwatch"
+)
+
+// Nagios exit codes
+const (
+	UNKNOWN  = 3
+	CRITICAL = 2
+	OK       = 0
+)
+
+func usage() {
+	fmt.Println("Usage: ./cwaq -i <instance-id> -m <metric> [-n <namespace]")
+}
+
+func main() {
+
+	// get cli args
+	idFlag := flag.String("i", "", "Instance ID of the host to check")
+	metricFlag := flag.String("m", "", "Metric to check for alarm")
+	namespaceFlag := flag.String("n", "AWS/EC2", "Namespace")
+
+	flag.Parse()
+
+	if *idFlag == "" || *metricFlag == "" {
+		usage()
+		os.Exit(-1)
+	}
+
+	// init aws session
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("us-west-2")},
+	)
+	checkerrF(err)
+
+	svc := cloudwatch.New(sess)
+
+	// build parameters for DescribeAlarmsForMetric call
+
+	// []*Dimension
+	var dims []*cloudwatch.Dimension
+	dims = append(dims, &cloudwatch.Dimension{
+		Name:  aws.String("InstanceId"),
+		Value: aws.String(*idFlag), // i-009614d62f6578510
+	})
+
+	params := cloudwatch.DescribeAlarmsForMetricInput{
+		Dimensions: dims,
+		MetricName: aws.String(*metricFlag),
+		Namespace:  aws.String(*namespaceFlag),
+		Period:     aws.Int64(300),
+		Statistic:  aws.String("Average"),
+	}
+
+	// make call
+	resp, err := svc.DescribeAlarmsForMetric(&params)
+	checkerrP(err)
+
+	// Check result and output nagios friendly exit codes
+	n := len(resp.MetricAlarms) - 1
+	if n >= 0 {
+		a := *resp.MetricAlarms[n].StateValue
+		if a == "ALARM" {
+			fmt.Println(a, "-", *resp.MetricAlarms[n].StateReason, "| state=1")
+			os.Exit(CRITICAL)
+		} else if a == "OK" {
+			fmt.Println(a, "| state=0")
+			os.Exit(OK)
+		} else {
+			fmt.Println(a, "-", *resp.MetricAlarms[n].StateReason, "| state=0")
+			os.Exit(UNKNOWN)
+		}
+	}
+
+}
+
+// helpers
+func checkerrF(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func checkerrP(e error) {
+	if e != nil {
+		fmt.Fprintf(os.Stderr, e.Error())
+		os.Exit(-1)
+	}
+}
